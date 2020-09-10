c0a04631e3c06c60035e05f44e5370b8a24528f5
fab679 <kamaufabisch1175@gmail.com>
2020-09-10 13:02:16 +0300

    my react portfolio app

--- src/components/body/Work.js
+++ src/components/body/Work.js
@@ -5,6 +5,7 @@ const Work = () => {
         <div className="my-10 leading-relaxed shadow-lg mx-6 text-center text-gray-600 text-sm">
             <p> At the moment i have no Projects going on... But i'm working on finding a good project. At the moment this page is more static but i'm working on making it more dynamic. I'll be adding some few dynamic applications that uses react and redux soon.... stay tuned i'll keep you posted. </p>
             <p>This site however is build with react alongside the css framework TailwindCSS. It is fully responsive to all devices. I built it as a My Portfolio to show case my skils.... I hope you like it if you are reading this. Feel free to email me for a job. </p>
+            <p>This site was created from a design from figma also designed by me which can be found <a href="https://www.figma.com/file/BIIBF723FtjalRHSLcILUm/Profile?node-id=9%3A14" target="_blank" rel="noopener noreferrer">Here</a></p>
         </div>
     );
 }
